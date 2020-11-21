9bbf117823d8c729b8b9e0a8b858d31ce36b54fa
Karita Shuhei <shoony.1015@gmail.com>
2020-11-21 23:25:14 +0900

    commit20201121_2325

---
+++ track_online/MenuViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MenuViewController.swift
+//  track_online
+//
+//  Created by 刈田修平 on 2020/11/21.
+//  Copyright © 2020 刈田修平. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet var menuView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // メニューの位置を取得する
+        let menuPos = self.menuView.layer.position
+        // 初期位置を画面の外側にするため、メニューの幅の分だけマイナスする
+        self.menuView.layer.position.x = -self.menuView.frame.width
+        // 表示時のアニメーションを作成する
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: {
+                self.menuView.layer.position.x = menuPos.x
+        },
+            completion: { bool in
+        })
+        
+    }
+
+    // メニューエリア以外タップ時の処理
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches {
+            if touch.view?.tag == 1 {
+                UIView.animate(
+                    withDuration: 0.2,
+                    delay: 0,
+                    options: .curveEaseIn,
+                    animations: {
+                        self.menuView.layer.position.x = -self.menuView.frame.width
+                },
+                    completion: { bool in
+                        self.dismiss(animated: true, completion: nil)
+                }
+                )
+            }
+        }
+    }
+}

cd6ecf97c112c041aff131a6e29e357dc918af78
Karita Shuhei <shoony.1015@gmail.com>
2020-11-23 18:47:33 +0900

    commit20201123_1847

--- track_online/premiumQAMovieAlertViewController.swift
+++ track_online/premiumQAMovieAlertViewController.swift
@@ -20,13 +20,25 @@ import AssetsLibrary
 class premiumQAMovieAlertViewController: UIViewController {
     @IBOutlet var approveFlagButton: UIButton!
     @IBOutlet var goToButton: UIButton!
-    
+    @IBOutlet var ImageView: UIImageView!
+    @IBOutlet var playVideo: UIButton!
+
+    let imagePickerController = UIImagePickerController()
+    var cache: String?
+    var videoURL: URL?
+    var playUrl:NSURL?
+    var data:Data?
+    var pickerview: UIPickerView = UIPickerView()
+
     var approveFlag:Int = 0
     let Ref = Database.database().reference()
 
     override func viewDidLoad() {
-        loadRuleText()
         goToButton.isEnabled = false
+        playVideo.isEnabled = false
+        loadRuleText()
+        loadMovie()
+        download()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -34,6 +46,45 @@ class premiumQAMovieAlertViewController: UIViewController {
         let ref1 = Ref.child("purchase").child("premium").child("setting").child("userRule")
 
     }
+    func loadMovie(){
+        let textImage:String = "movieAlert.png"
+        let refImage = Storage.storage().reference().child("purchase").child("premium").child("setting").child("movieAlert").child("\(textImage)")
+        ImageView.sd_setImage(with: refImage, placeholderImage: nil)
+        playVideo.addTarget(self, action: #selector(playVideo(_:)), for: .touchUpInside)
+
+    }
+    @objc func playVideo(_ sender: UIButton) {
+        let player = AVPlayer(url: playUrl! as URL
+        )
+        
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        present(controller, animated: true) {
+            controller.player!.play()
+        }
+    }
+    
+    func download(){
+        let textVideo:String = "movieAlert.mp4"
+        let refVideo = Storage.storage().reference().child("purchase").child("premium").child("setting").child("movieAlert").child("\(textVideo)")
+        refVideo.downloadURL{ [self] url, error in
+            if (error != nil) {
+            } else {
+                self.playUrl = url as NSURL?
+                print("download success!! URL:", url!)
+            }
+            playVideo.isEnabled = true
+        }
+        //        if self.cache == "1"{
+        //            SDImageCache.shared.clearMemory()
+        //            SDImageCache.shared.clearDisk()
+        //            let ref0 = Database.database().reference().child("QA").child("\(currentUid)").child("private").child("\(text!)")
+        //            let data = ["cache":"0" as Any] as [String : Any]
+        //            ref0.updateChildValues(data)
+        //        }
+    }
+
     @IBAction func tapApproveFlagButton(_ sender: Any) {
         if approveFlag == 0{
             approveFlag = 1
